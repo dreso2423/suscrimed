d61f31010dd1f05e77b742a147ce741cf8fadce5
andre sottil <dreso2423@gmail.com>
2021-02-06 15:24:26 -0600

    good

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  get 'drugs/index'
+  get 'drugs/new'
+  get 'drugs/create'
+  get 'drugs/show'
+  get 'drugs/edit'
+  get 'drugs/update'
+  get 'drugs/delete'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

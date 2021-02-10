c73b32a11f9b9d60693c3e522b0250db2bef07b9
andre sottil <dreso2423@gmail.com>
2021-02-09 20:08:24 -0600

    front end

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,9 @@ Rails.application.routes.draw do
   get 'invoices/delete'
   get 'invoices/create'
   get 'invoices/new'
+  resources :invoices do
+    resources :invoice_details
+  end
   resources :drugs, only: :index
 
   devise_for :users

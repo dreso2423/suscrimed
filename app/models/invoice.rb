9edf15a1d044395ac45585cab2a564fe47959e41
dominic lizarraga <dominiclizarraga@hotmail.com>
2021-02-09 19:48:26 -0800

    puntdit

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -1,4 +1,15 @@
 class Invoice < ApplicationRecord
   belongs_to :user_id
   has_many :invoice_details
+
+  FREQUENCY = [
+
+    "Only one",
+    "Twice per month",
+    "Monthly",
+    "Every 3 months"
+    ]
+
+    validates :frequency,
+              inclusion: { in: FREQUENCY }
 end

8406aaebc083994b7b05d642f687ad4c668769fa
andre sottil <dreso2423@gmail.com>
2021-02-16 06:48:47 -0600

    good

--- app/models/drug.rb
+++ app/models/drug.rb
@@ -1,4 +1,10 @@
 class Drug < ApplicationRecord
   has_many :invoice_details
   monetize :price_cents
+  include PgSearch::Model
+    pg_search_scope :search_by_drugs,
+      against: [ :drug_name, :description,:ean,:title],
+      using: {
+        tsearch: { prefix: true } # <-- now `superman batm` will return something!
+      }
 end

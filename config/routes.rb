798556983a76edb283ea14598b25a3a99b8a5b2c
dominic lizarraga <dominiclizarraga@hotmail.com>
2021-02-16 19:52:01 -0800

    boton eliminar trabjando

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,6 @@
 Rails.application.routes.draw do
-  get 'invoice_details/index'
-  get 'invoice_details/edit'
-  get 'invoice_details/create'
-  get 'invoice_details/show'
-  get 'invoice_details/update'
-  get 'invoice_details/delete'
-  get 'invoices/index'
-  get 'invoices/show'
-  get 'invoices/edit'
-  get 'invoices/update'
-  get 'invoices/delete'
-  get 'invoices/create'
-  get 'invoices/new'
+  root to: 'pages#home'
+
   resources :invoices do
     resources :invoice_details
   end
@@ -19,10 +8,11 @@ Rails.application.routes.draw do
   post 'drugs/add_to_invoice'
 
   # si es por post se cambia la linea
+  delete "session/invoice_detail/drugs/:id", to: "invoice_details#delete"
 
   devise_for :users,
       controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
-  root to: 'pages#home'
+
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

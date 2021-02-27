8a3c3b00d7f7e644860f0320b3498527639076f2
andre sottil <dreso2423@gmail.com>
2021-02-27 11:07:26 -0600

    good

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,7 +2,6 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
-
 require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
@@ -10,15 +9,8 @@ require("channels")
 require("jquery")
 import "bootstrap"
 require("channels")
-
-
 /* JAVASCCRIPT FOR COMPONENTS */
 require("../components/brand-logos")
-require("../components/cart")
-require("../components/form")
-
-
-
 $(document).on('turbolinks:load', function(){
   $('.dropdown').on("click",function(){
     if($(this).hasClass('show')){
@@ -31,10 +23,3 @@ $(document).on('turbolinks:load', function(){
     return true;
   })
 })
-
-// Uncomment to copy all static images under ../images to the output folder and reference
-// them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
-// or the `imagePath` JavaScript helper below.
-//
-// const images = require.context('../images', true)
-// const imagePath = (name) => images(name, true)
